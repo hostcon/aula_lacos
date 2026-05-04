a8df8bb0ab7a1db112bf37a67ff64d29aeacc5e5
wagaum <138220357+hostcon@users.noreply.github.com>
2026-05-04 19:47:58 -0300

    Pronto

--- lacos.py
+++ lacos.py
@@ -1,1 +1,10 @@
-print("Isso é fácil")
+idade = int(input("Digite sua idade: "))
+
+if idade < 13:
+    print("Criança")
+elif idade < 18:
+    print("Adolescente")
+elif idade < 65:
+    print("Adulto")
+else:
+    print("Idoso")
